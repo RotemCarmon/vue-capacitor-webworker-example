ab6754329fda3aa4bfd1ecf3e0c44ce36aa8645e
RotemCarmon <tetch6@gmail.com>
2024-11-08 14:45:18 +0200

    add capacitor

---
+++ android/app/src/main/java/com/example/vue_capacitor_webworker/MainActivity.java
@@ -0,0 +1,5 @@
+package com.example.vue_capacitor_webworker;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.vue_capacitor_webworker',
+  appName: 'vue-capacitor-webworker',
+  webDir: 'dist'
+};
+
+export default config;
